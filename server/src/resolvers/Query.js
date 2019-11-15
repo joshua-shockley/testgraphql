763ef2054c32981e18293083506531df2963401f
Joshua-Shockley <thelumbashockley@gmail.com>
2019-11-14 23:33:24 -0600

    more prep to get to pull link and user by id.. running into a snag there when researching

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -26,13 +26,22 @@ async function feed(parent, args, context) {
         links,
     }
 }
-
+//my attempt at putting item by id into the query items to pull up a view of the individual item/person/link
 async function allUsers(parent, args, context, info) {
     return context.prisma.users()
 }
 
+function oneLink(parent, args, context, info) {
+    return context.prisma.link({ id: args.id })
+}
+
+function oneUser(parent, args, context, info) {
+    return context.prisma.user({ id: args.id })
+}
 
 module.exports = {
     feed,
     allUsers,
+    oneLink,
+    oneUser,
 }
